58a57479aef50b384bb34304aa92ac21083f1fdd
parvpatodi <parvpatodi123@gmail.com>
2017-07-28 21:27:52 +0530

    Linking association between articles and categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true,
                     length: { minimum: 3 }
   validates :description, presence: true
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728142219) do
+ActiveRecord::Schema.define(version: 20170728155625) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
